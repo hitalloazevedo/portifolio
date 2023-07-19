102278e844ddaef9eddc6b9c7c8c0e9335ce1935
HitalloAzevedo <azevedohitallo@gmail.com>
2023-07-19 14:19:17 -0300

    corrigindo referencias e variaveis

--- js/outputAnimation.js
+++ js/outputAnimation.js
@@ -1,21 +1,20 @@
 const output = document.querySelector('#console-output')
 
 async function typeWriter(text, element) {
-    const textArray = text.split('')
-    const stringReverse = [...textArray]
-    stringReverse.reverse()
+    let textArray = text.split('')
+    let stringReverse = [...textArray].reverse()
 
     while (true) {
         for (let l in textArray) {
             await delay(60)
-            output.textContent += textArray[l]
+            element.textContent += textArray[l]
         }
     
         await delay(2000)
         
         for (let l in stringReverse) {
             await delay(40)
-            output.textContent = output.textContent.replace(stringReverse[l], '')
+            element.textContent = element.textContent.replace(stringReverse[l], '')
         }
 
         await delay(2000)
@@ -26,4 +25,4 @@ function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time))
 }
 
-typeWriter('Welcome to my portifolio website!' ,output)
+typeWriter('Welcome to my portifolio website!', output)
