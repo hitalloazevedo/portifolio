fda030240011f3ef463835e613df5d13e60d44db
HitalloAzevedo <azevedohitallo@gmail.com>
2023-10-23 20:57:39 -0300

    style: fix home page to mobile

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,14 @@
 const menuIcon = document.querySelector('#menu-icon')
 const menuOptions = document.querySelector('ul.menus')
+const home = document.querySelector('#home')
 
 menuIcon.addEventListener('click', () => {
     if (menuOptions.style.display == 'block') {
         menuOptions.style.display = 'none'
+        home.style.paddingTop = '0px'
     } else {
         menuOptions.style.display = 'block'
+        home.style.paddingTop = '100px'
     }
 })
 
