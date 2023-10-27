a7b56e6f1502e7fb166c8c9623c1e78847733a3e
HitalloAzevedo <azevedohitallo@gmail.com>
2023-10-27 10:58:56 -0300

    media: 480-768, header done and add responsivity to menus

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ const menuOptions = document.querySelector('ul.menus')
 const home = document.querySelector('#home')
 
 menuIcon.addEventListener('click', () => {
-    if (menuOptions.style.display == 'block') {
+    if (menuOptions.style.display == 'block' && window.innerWidth <= 480) {
         menuOptions.style.display = 'none'
     } else {
         menuOptions.style.display = 'block'
@@ -11,6 +11,18 @@ menuIcon.addEventListener('click', () => {
 })
 
 menuOptions.addEventListener('click', () => {
-    menuOptions.style.display = 'none'
+    if (window.innerWidth <= 480){
+        menuOptions.style.display = 'none'
+    }
+    else if (window.innerWidth >= 481){
+        menuOptions.style.display = 'flex'
+    }
 })
 
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 481 && window.innerWidth <= 768) {
+        menuOptions.style.display = 'flex'
+    } else {
+        menuOptions.style.display = 'none'
+    }
+})
